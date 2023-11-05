114431cc3e883513f77668af1b49ace0aa75110f
elizomak <elizomak@fit.cvut.cz>
2023-11-05 16:52:56 +0100

    Input Handler: Simple exceptions and loop structure made

--- src/main/scala/Console/InputHandler.scala
+++ src/main/scala/Console/InputHandler.scala
@@ -1,6 +1,22 @@
 package Console
+import Image.Image
+import Filter.{Filter, FlipFilter, FontFilter, InvertFilter, RotateFilter, ScaleFilter}
 
-class InputHandler {
+class InputHandler(args: Array[String]) {
 
-  // TODO
+  // We need at least one image argument for correct working
+  if (args.isEmpty) {
+    throw new Exception("No arguments in input.")
+  }
+
+  private val filters: List[Filter] = List()
+
+    for (arg <- args) {
+      // Arguments must come in pairs, where the first argument must begin with "--"
+      if (arg.length() <= 2 || !(arg[0] != '-' || arg[1] != '-')) {
+        throw new Exception("Invalid argument.")
+      }
+
+      // TODO
+    }
 }
