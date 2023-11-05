1750db7b5a207760ca7c83957e28c1f39456e333
elizomak <elizomak@fit.cvut.cz>
2023-11-05 18:24:54 +0100

    Fix tests with updated class InputHandler without exceptions

--- src/test/scala/Console/InputHandlerTest.scala
+++ src/test/scala/Console/InputHandlerTest.scala
@@ -4,16 +4,12 @@ import org.scalatest.FunSuite
 class InputHandlerTest extends FunSuite {
 
   test("No arguments in input") {
-    val exception = intercept[Exception] {
-      val input = new InputHandler(Array())
-    }: Exception
-    assert(exception.getMessage() == "No arguments in input.")
+    val input = new InputHandler()
+    assert(!input.handle(Array()))
   }
 
   test("Invalid arguments in input") {
-    val exception = intercept[Exception] {
-      val input = new InputHandler(Array("bzz", "bzz"))
-    } : Exception
-    assert(exception.getMessage() == "Invalid argument.")
+    val input = new InputHandler()
+    assert(!input.handle(Array("bzz", "bzz")))
   }
 }
