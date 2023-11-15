7814331240f959a30cb7c4f1a5089891c258dd86
elizomak <elizomak@fit.cvut.cz>
2023-11-15 13:37:41 +0100

    Implement random image generation in RandomImage class

--- src/main/scala/Image/RandomImage.scala
+++ src/main/scala/Image/RandomImage.scala
@@ -1,11 +1,33 @@
 package Image
-import Image.Image
-
 import java.awt.image.BufferedImage
+import java.awt.Color
+import java.util.Random
 
 class RandomImage(height: Int, width: Int) extends Image {
 
-  override def get: Option[BufferedImage] = ??? // TODO
+  private val image: BufferedImage = this.generateImage
+
+  private def generateImage: BufferedImage = {
+    val returnImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    val random = new Random()
+
+    for (x <- 0 until width) {
+      for (y <- 0 until height) {
+        val red = random.nextInt(256) // Random red component (0-255)
+        val green = random.nextInt(256) // Random green component (0-255)
+        val blue = random.nextInt(256) // Random blue component (0-255)
+
+        val rgb = new Color(red, green, blue).getRGB
+
+        returnImage.setRGB(x, y, rgb)
+      }
+    }
+    returnImage
+  }
+  override def get: Option[BufferedImage] = Some(generateImage)
 
   override def getSize: Option[(Int, Int)] = Some((height, width))
+
+
+
 }
