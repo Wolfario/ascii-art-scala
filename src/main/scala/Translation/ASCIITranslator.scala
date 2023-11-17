d89b8fdb1f3d63f3792a9af8dcf542f3df41f5c8
elizomak <elizomak@fit.cvut.cz>
2023-11-17 18:08:18 +0100

    Implement greyscale calculation function and linear and nonlinear switching

--- src/main/scala/Translation/ASCIITranslator.scala
+++ src/main/scala/Translation/ASCIITranslator.scala
@@ -1,9 +1,39 @@
 package Translation
+import java.awt.Color
 import Image.Image
 import Translation.Type.{Translation, LinearTranslation, NonLinearTranslation}
 
-class ASCIITranslator(Image: Image) {
+class ASCIITranslator(Image: Image, ASCIISequence: Seq[Char]) {
 
   // Linear translation set as default
   private var translationType: Translation = new LinearTranslation()
+
+  def caclulateGreyscale(image: Image): Array[Array[Int]] = {
+    image.get match {
+      case Some(img) =>
+        val width = img.getWidth
+        val height = img.getHeight
+        val greyscale_array = Array.ofDim[Int](width, height)
+
+        for {
+          x <- 0 until width
+          y <- 0 until height
+        } {
+          val pixel = new Color(img.getRGB(x, y))
+          val red = pixel.getRed
+          val green = pixel.getGreen
+          val blue = pixel.getBlue
+          val grey: Int = (0.3 * red + 0.59 * green + 0.11 * blue).toInt
+          greyscale_array(y)(x) = grey
+        }
+        greyscale_array
+
+      case None => Array.empty
+    }
+  }
+
+  def useLinearTranslation(): Unit = translationType = new LinearTranslation()
+
+  def useNonLinearTranslation(): Unit = translationType = new NonLinearTranslation()
+
 }
