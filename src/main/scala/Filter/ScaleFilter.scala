5cb9093fcd36652b1ae8989501e082846a0f9658
elizomak <elizomak@fit.cvut.cz>
2023-12-05 19:58:49 +0100

    Change implementation of ScaleFilter for grayscale image processing

--- src/main/scala/Filter/ScaleFilter.scala
+++ src/main/scala/Filter/ScaleFilter.scala
@@ -1,10 +1,9 @@
 package Filter
-import Image.{Image, ImportedImage}
-import java.awt.image.BufferedImage
+import Image.GreyscaleImage
 
 class ScaleFilter(coefficient: Float) extends Filter {
 
-  override def apply(image: Image): Image = {
+  override def apply(image: GreyscaleImage): GreyscaleImage = {
     if (coefficient == 1.0) {
       return image
     }
@@ -19,39 +18,37 @@ class ScaleFilter(coefficient: Float) extends Filter {
     }
   }
 
-  private def scaleQuarter(image: Image): Image = {
+  private def scaleQuarter(image: GreyscaleImage): GreyscaleImage = {
     val srcWidth = image.getSize._2
     val srcHeight = image.getSize._1
     val destWidth = (srcWidth * 0.25).toInt
     val destHeight = (srcHeight * 0.25).toInt
-    val destImage = new ImportedImage(destHeight, destWidth, new BufferedImage(destWidth, destHeight, image.get.getType))
+    val destImage = Array.ofDim[Int](destHeight, destWidth)
 
     for (x <- 0 until destWidth; y <- 0 until destHeight) {
       val srcX = x * 4
       val srcY = y * 4
 
-      val srcColor = image.get.getRGB(srcX, srcY)
-      destImage.get.setRGB(x, y, srcColor)
+      destImage(y)(x) = image.getGreyscale(srcY)(srcX)
     }
-    destImage
-  }
+    new GreyscaleImage(destHeight, destWidth, destImage)
+}
 
-  private def scaleFour(image: Image): Image = {
+  private def scaleFour(image: GreyscaleImage): GreyscaleImage = {
     val srcWidth = image.getSize._2
     val srcHeight = image.getSize._1
     val destWidth = srcWidth * 4
     val destHeight = srcHeight * 4
-    val destImage = new ImportedImage(destHeight, destWidth, new BufferedImage(destWidth, destHeight, image.get.getType))
+    val destImage = Array.ofDim[Int](destHeight, destWidth)
 
     for (x <- 0 until srcWidth; y <- 0 until srcHeight) {
       val srcX = x * 4
       val srcY = y * 4
-      val srcColor = image.get.getRGB(x, y)
 
       for (i <- 0 until 4; j <- 0 until 4) {
-        destImage.get.setRGB(srcX + i, srcY + j, srcColor)
+        destImage(srcY + i)(srcX + j) = image.getGreyscale(y)(x)
       }
     }
-    destImage
+    new GreyscaleImage(destHeight, destWidth, destImage)
   }
 }
