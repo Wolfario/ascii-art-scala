fc18c58bd42637557a933554efd4162726843267
elizomak <elizomak@fit.cvut.cz>
2023-12-05 16:27:34 +0100

    Implement x4 scaling in scale filter

--- src/main/scala/Filter/ScaleFilter.scala
+++ src/main/scala/Filter/ScaleFilter.scala
@@ -37,7 +37,21 @@ class ScaleFilter(coefficient: Float) extends Filter {
   }
 
   private def scaleFour(image: Image): Image = {
-    image
-    // TODO: Complain
+    val srcWidth = image.getSize._2
+    val srcHeight = image.getSize._1
+    val destWidth = srcWidth * 4
+    val destHeight = srcHeight * 4
+    val destImage = new ImportedImage(destHeight, destWidth, new BufferedImage(destWidth, destHeight, image.get.getType))
+
+    for (x <- 0 until srcWidth; y <- 0 until srcHeight) {
+      val srcX = x * 4
+      val srcY = y * 4
+      val srcColor = image.get.getRGB(x, y)
+
+      for (i <- 0 until 4; j <- 0 until 4) {
+        destImage.get.setRGB(srcX + i, srcY + j, srcColor)
+      }
+    }
+    destImage
   }
 }
