d4f1a837b25e7b998f58c22dbbe8484a78003d92
elizomak <elizomak@fit.cvut.cz>
2023-12-13 13:54:33 +0100

    Delete FilterConfigurator due uselessness

--- src/main/scala/Filter/FilterConfigurator/FilterConfigurator.scala
+++
@@ -1,8 +0,0 @@
-package Filter.FilterConfigurator
-import Image.Image
-import Filter.{Filter, BrightnessFilter, FlipFilter, FontFilter, InvertFilter, RotateFilter, ScaleFilter}
-
-class FilterConfigurator {
-
-  // TODO
-}
