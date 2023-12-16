bffb44be11c990a15f301e531ae9f61dd72d6dee
elizomak <elizomak@fit.cvut.cz>
2023-12-16 17:19:12 +0100

    Fix typo and add empty object comparing to equal method

--- src/main/scala/Image/GreyscaleImage.scala
+++ src/main/scala/Image/GreyscaleImage.scala
@@ -1,16 +1,19 @@
 package Image
 import java.awt.image.BufferedImage
 
-class GreyscaleImage(height: Int, width: Int, grayscaleImage: Array[Array[Int]]) extends Image {
+class GreyscaleImage(height: Int, width: Int, greyscaleImage: Array[Array[Int]]) extends Image {
 
 
   override def get: BufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
   override def getSize: (Int, Int) = (height, width)
 
-  def getGreyscale: Array[Array[Int]] = grayscaleImage
+  def getGreyscale: Array[Array[Int]] = greyscaleImage
 
   def equals(other: GreyscaleImage): Boolean = {
+    if ((getSize == (0,0)) && (other.getSize == (0,0)))
+      return true
+
     val thisGreyscale = getGreyscale
     val otherGreyscale = other.getGreyscale
 
