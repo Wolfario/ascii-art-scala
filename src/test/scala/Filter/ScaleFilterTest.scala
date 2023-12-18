a3d54507bfd04684d2676f01d1b699d8b3a593cf
elizomak <elizomak@fit.cvut.cz>
2023-12-18 17:25:32 +0100

    Add more tests for SCaleFilterTest

--- src/test/scala/Filter/ScaleFilterTest.scala
+++ src/test/scala/Filter/ScaleFilterTest.scala
@@ -25,4 +25,25 @@ class ScaleFilterTest extends FunSuite {
 
     assert(greyscale.getSize == filteredGreyscale.getSize)
   }
+
+  test("Applying scale filter with all available values.") {
+    val importer = new ImageImporterImageIO
+    val greyscaler = new GreyscaleTranslator(importer.loadFrom("testfiles/bunny.png"))
+    val greyscale = greyscaler.translate()
+
+    val scaleFilterUp = new ScaleFilter(4)
+    val scaleFilterDown = new ScaleFilter(0.25.toFloat)
+    val scaleFilterSame = new ScaleFilter(1)
+
+    var filteredResult = scaleFilterSame.apply(greyscale)
+    assert(greyscale.getSize == filteredResult.getSize)
+
+    filteredResult = scaleFilterUp.apply(greyscale)
+    val upTuple = (greyscale.getSize._1 * 4, greyscale.getSize._2 * 4)
+    assert(upTuple == filteredResult.getSize)
+
+    filteredResult = scaleFilterDown.apply(greyscale)
+    val downTuple = (greyscale.getSize._1 * 0.25, greyscale.getSize._2 * 0.25)
+    assert(downTuple == filteredResult.getSize)
+  }
 }
