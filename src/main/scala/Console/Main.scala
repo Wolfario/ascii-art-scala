ef0f4d97d544165d25fd7cc1d414bf0e466e2736
elizomak <elizomak@fit.cvut.cz>
2023-12-19 18:07:53 +0100

    Implement Main scala file that connect InputHandler and Application classes and lauch translation

--- src/main/scala/Console/Main.scala
+++ src/main/scala/Console/Main.scala
@@ -1,5 +1,18 @@
 package Console
+import Application.Application
 
 object Main extends App {
-  println("Hello there")
+
+  var consoleInput: InputHandler = new InputHandler
+  consoleInput.handle(args)
+
+  var app = new Application
+
+  app.setImage(consoleInput.getImage)
+  app.setFilters(consoleInput.getFilters)
+  app.setTable(consoleInput.getTable)
+  app.setTranslationType(consoleInput.getTranslationType)
+  app.setOutput(consoleInput.getOutput)
+
+  app.handle()
 }
