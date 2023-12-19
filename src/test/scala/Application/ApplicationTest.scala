fd4b84665c63979a0d2051f7f89c1c25be0edd05
elizomak <elizomak@fit.cvut.cz>
2023-12-19 18:04:36 +0100

    Implement tests for application class

---
+++ src/test/scala/Application/ApplicationTest.scala
@@ -0,0 +1,33 @@
+package Application
+import Image.ImageRGB.RandomImage
+import Output.ASCII.ASCIIOutputFile
+import org.scalatest.FunSuite
+
+class ApplicationTest extends FunSuite {
+  test("Invalid applying handle without set input.") {
+    val exception = intercept[Exception] {
+      val app = new Application
+      app.handle()
+    }
+    assert(exception.getMessage == "Set image for translation.")
+  }
+
+  test("Invalid applying handle without set output.") {
+    val exception = intercept[Exception] {
+      val app = new Application
+      app.setImage(new RandomImage(150, 150))
+      app.handle()
+    }
+    assert(exception.getMessage == "Cannot use empty output methods. Set correct output.")
+  }
+
+  test("Pass correct linear translation of RandomImage to file.") {
+    val app = new Application
+    app.setImage(new RandomImage(150, 150))
+    app.setTable((true, "ten_levels"))
+    app.setTranslationType(false)
+    app.setTranslationType(true)
+    app.setOutput(new ASCIIOutputFile("testfiles/output.txt"))
+    app.handle()
+  }
+}
