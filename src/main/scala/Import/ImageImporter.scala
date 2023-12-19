7aa56c811cc37e45dd9f2b657d79b1d20ef35ad0
elizomak <elizomak@fit.cvut.cz>
2023-12-19 18:28:16 +0100

    Add docstrings for all Import classes

--- src/main/scala/Import/ImageImporter.scala
+++ src/main/scala/Import/ImageImporter.scala
@@ -2,7 +2,13 @@ package Import
 
 import Image.ImageRGB.ImportedImage
 
+/**
+ * A trait for loading images from file sources.
+ */
 trait ImageImporter[T] {
 
+  /**
+   * Loads an image from the specified source.
+   */
   def loadFrom(source: T): ImportedImage
 }
--- src/main/scala/Import/ImageImporterImageIO.scala
+++ src/main/scala/Import/ImageImporterImageIO.scala
@@ -1,5 +1,4 @@
 package Import
-
 import Image.ImageRGB.ImportedImage
 import java.awt.image.BufferedImage
 import java.io.File
@@ -7,8 +6,18 @@ import java.io.FileNotFoundException
 import java.io.IOException
 import javax.imageio.ImageIO
 
+/**
+ * An ImageImporter implementation using ImageIO for loading images from file paths.
+ */
 class ImageImporterImageIO() extends ImageImporter[String] {
 
+  /**
+   * Loads an image from the specified file path using ImageIO.
+   *
+   * @param source The file path of the image to load.
+   * @return An ImportedImage object representing the loaded image.
+   * @throws Exception If there is an issue opening or reading the file.
+   */
   def loadFrom(source: String): ImportedImage = {
     try {
       val imageFile: File = new File(source)
